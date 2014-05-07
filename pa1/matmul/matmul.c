362472a67a5684d81db9d0bfee53d89a8f111fa3
Yang Li <drink-0.Stanford.EDU>
2014-05-06 18:49:38 -0700

    implemented strassen, bugged

--- pa1/matmul/matmul.c
+++ pa1/matmul/matmul.c
@@ -10,6 +10,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <pthread.h>
 
 #define NUM_THREADS 4
@@ -21,13 +22,33 @@
 #if 1
 #define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
 
-void matmul_strassen_leaf(const double* A, 
-                          const double* B, 
-                          double* C);
-void matmul_strassen(int N, 
+void matmul_strassen_leaf(int r0, int c0, int N, const double* AA, 
+                          const double* BB,  double* CC);
+
+  void matmul_strassen(int r0, int c0, int n,  int N, 
                      const double* A, 
                      const double* B, 
                      double* C);
+
+int isPowerOfTwo(int x){
+   return x && (!(x &(x-1)));
+}
+
+int divides(int x, int y){
+  return x%y == 0;
+}
+
+void print_mat(int N, const double *C){
+  int k,j;
+  for ( k=0; k<N;k++){
+    for ( j=0; j<N;j++){
+      //printf("[%i,%i] %i = %f\n",k,j,C[k*N+j],k*N+j);
+      printf("%f ",k,j,C[k*N+j],k*N+j);
+    }
+    printf("\n");
+  }
+  printf("\n");
+}
 
 typedef struct {
    const double *A;
@@ -40,6 +61,16 @@ typedef struct {
    int col_end;
 } thread_arg;
 
+void matmul1(int N, const double* A, const double* B, double* C) {
+  int i, j, k;
+
+  for (i = 0; i < N; i++)
+    for (j = 0; j < N; j++)
+      for (k = 0; k < N; k++)
+        C[i*N + j] += A[i*N + k] * B[k*N + j];
+}
+
+
 void matmul_strassen_test(const double* A, const double* B,  double* C){
   C[0]+= A[0]*B[0] + A[1]*B[2];
   C[1]+= A[0]*B[1] + A[1]*B[3];
@@ -47,9 +78,22 @@ void matmul_strassen_test(const double* A, const double* B,  double* C){
   C[3]+= A[2]*B[1] + A[3]*B[3];
 }
 
-void matmul_strassen_leaf(const double* A, const double* B,  double* C){
+void matmul_strassen_leaf(int r0, int c0, int N, const double* AA, 
+                          const double* BB,  double* C){
   // unsafe operations assuming A, B and C has size 4
   double m1,m2,m3,m4,m5,m6,m7;
+  
+  double A[4] = {AA[r0*N + c0],AA[r0*N +c0+1],
+                 AA[(r0+1)*N+c0], AA[(r0+1)*N+c0+1]};
+  double B[4] = {BB[r0*N + c0], BB[r0*N +c0+1],
+                 BB[(r0+1)*N+c0], BB[(r0+1)*N+c0+1]};
+  /*double GT[4] = {C[r0*N + c0], C[r0*N +c0+1],
+                 C[(r0+1)*N+c0], C[(r0+1)*N+c0+1]};*
+  matmul_strassen_test(&A, &B, &GT);*/
+
+  printf("leaf index: r0=%i c0=%i N=%i [%i %i; %i %i]\n",
+         r0,c0,N,
+         r0*N+c0, r0*N+c0+1,(r0+1)*N+c0, (r0+1)*N + c0+1);
   m1 = (A[0]+A[3])*(B[0]+B[3]);
   m2 = (A[2]+A[3])*B[0];
   m3 = A[0]*(B[1]-B[3]);
@@ -58,26 +102,80 @@ void matmul_strassen_leaf(const double* A, const double* B,  double* C){
   m6 = (A[2]-A[0])*(B[0]+B[1]);
   m7 = (A[1]-A[3])*(B[2]+B[3]);
   //  C[0] = 0; C[1]=0;C[2]=0;C[3] = 0;
-  C[0] += m1+m4-m5+m7;
-  C[1] += m3+m5;
-  C[2] += m2+m4;
-  C[3] += m1-m2+m3+m6; 
+  printf("C: [%i %i; %i %i] = [%f %f; %f %f]\n",
+         r0*N+c0, r0*N+c0+1,(r0+1)*N+c0, (r0+1)*N+c0+1,
+         C[r0*N+c0], C[r0*N+c0+1], C[(r0+1)*N+c0], C[(r0+1)*N + c0+1]);
+
+  C[r0*N + c0] += m1+m4-m5+m7;
+  C[r0*N + c0 +1] += m3+m5;
+  C[(r0+1)*N +c0] += m2+m4;
+  C[(r0+1)*N+c0+1] += m1-m2+m3+m6; 
+  printf("C: [%i %i; %i %i] = [%f %f; %f %f]\n",
+         r0*N+c0, r0*N+c0+1,(r0+1)*N+c0, (r0+1)*N+c0+1,
+         C[r0*N+c0], C[r0*N+c0+1], C[(r0+1)*N+c0], C[(r0+1)*N + c0+1]);
+  //printf("C_gt: [%f %f; %f %f]\n",
+  //       GT[0], GT[1], GT[2], GT[3]);
 }
 
-void matmul_strassen(int N , const double* A, const double* B, 
+/**
+ * function: matmul_strassen
+ * 
+ * r0: min row index of current partition
+ * c0: min col index of current partition
+ * n: current partition size
+ * N: size of A,B and C
+ */
+
+void matmul_strassen(int r0, int c0, int n, int N , const double* A, 
+                     const double* B, 
                      double* C){
-  if (N==2){
-    printf("C = [%f %f; %f %f]\n",C[0],C[1],C[2],C[3]);
-    
-    matmul_strassen_leaf(A,B,C);
-    printf("C' = [%f %f; %f %f]\n",C[0],C[1],C[2],C[3]);
+  int s = N*N;
+  
+  //double C_copy[s];
+  //memcpy(&C_copy,C,sizeof(double)*s);
+  if (n==2){
+    //printf("C = [%f %f; %f %f]\n",C[0],C[1],C[2],C[3]);
+    matmul_strassen_leaf(r0,c0,N,A,B,C);
+    //printf("C' = [%f %f; %f %f]\n",C[0],C[1],C[2],C[3]);
+  }else if(isPowerOfTwo(n)){
+    int new_n = n/2;
+    printf("r0 = %i, c0 = %i, n = %i, N = %i , new n = %i\n",
+           r0,c0,n,N,new_n);
+
+  print_mat(N,C);   
+    matmul_strassen(r0,c0,new_n,N,A,B,C);
+  print_mat(N,C);      
+  
+  matmul_strassen(r0,c0+new_n,new_n,N,A,B,C);
+  print_mat(N,C);   
+    matmul_strassen(r0+new_n,c0,new_n,N,A,B,C);
+print_mat(N,C);   
+    matmul_strassen(r0+new_n,c0+new_n, new_n,N,A,B,C);
+print_mat(N,C);   
   }
+  
+  
+    //matmul1(N,A,B,C_copy);
+  
+  
+
+  
 }
 
-void matmul(int N, const double*__restrict__ A, const double*__restrict__ B, double* __restrict__ C) {
-  if (N==2){
-    matmul_strassen(2,A,B,C);
-  }return;
+
+
+
+
+void matmul(int N, const double*__restrict__ A, const double*__restrict__
+B, double* __restrict__ C) {
+  print_mat(N,A);
+
+  print_mat(N,B);
+  //if (N==2){
+    matmul_strassen(0,0,N,N,A,B,C);
+    return;
+    //}
+
 
 
 /* matrix transpose */
